f06a416558cf3d440f14fb3b9d286ce9f97f6f36
DatchinaMurthy S <datchina.murthy77@gmail.com>
2024-03-11 13:40:00 +0530

    Initial Commit

---
+++ src/main/java/com/aws/deployment/AwsDeploymentsApplication.java
@@ -0,0 +1,13 @@
+package com.aws.deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeploymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeploymentsApplication.class, args);
+	}
+
+}
